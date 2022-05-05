aed25add7f617343c3afcae711627c3f0b4fe145
Bruno Landim <brunolandim13@hotmail.com>
2022-05-05 16:13:46 -0300

    requisito 6

---
+++ src/Fighter.ts
@@ -0,0 +1,13 @@
+import Energy from './Energy';
+
+export default interface Figther {
+  lifePoints:number;
+  strength:number;
+  defense :number;
+  energy?:Energy;
+
+  attack(enemy:Figther):void
+  special(enemy:Figther):void 
+  levelUp():void 
+  receiveDamage(attackPoints:number):void 
+}
